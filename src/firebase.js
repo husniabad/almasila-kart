f001bd86dcd6e27beef53450c524383dc686e180
Husni Abad <husni.abad.97@gmail.com>
2023-02-16 15:19:01 +0530

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB6-4TBeMuRHXjJhTNXfE3Mg8z5142gHbs",
+    authDomain: "almasila-kart.firebaseapp.com",
+    projectId: "almasila-kart",
+    storageBucket: "almasila-kart.appspot.com",
+    messagingSenderId: "998856417418",
+    appId: "1:998856417418:web:6d1eadaa8db84aa0388d73",
+    measurementId: "G-1M7QMM2ZRE"
+  };
